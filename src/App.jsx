85d3d9519e9f9be2cd7d6cb0fb4ea4a65746ae5b
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-08-20 11:09:50 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ export default class App extends Component {
     count: 0,
   };
   plusBtn = () => {
+    const {count} = this.state
     this.setState({
-      count: this.state.count + 1,
+      count: count + 1,
     });
   };
   minusBtn = () => {
+    const {count} = this.state
     this.setState({
-      count: this.state.count - 1,
+      count: count - 1,
     });
   };
   render() {
+    const {count} = this.state
     return (
       <div className="container">
-<div className="d-flex gap-3 mt-4">
+<div className="d-flex gap-3 mt-4 juctify-center">
         <button className="btn btn-primary" onClick={this.plusBtn}>+</button>
-        <h1>{this.state.count}</h1>
+        <h1>{count}</h1>
         <button className="btn btn-danger" onClick={this.minusBtn}>-</button>
       </div>
       </div>
